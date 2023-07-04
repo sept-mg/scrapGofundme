16a4d51160cc940ae7b28b17325cc161da88f02b
SEPT Mg <67376828+sept-mg@users.noreply.github.com>
2023-07-04 02:08:18 +0200

    fzezef

--- index.php
+++ index.php
@@ -10,8 +10,19 @@ if (isset($_GET['page'])) {
     ));
 
     if ($nbpage !== false) {
-        // La valeur de 'page' est un entier valide entre 1 et 1000
-        echo "La valeur de 'page' est : " . $nbpage;
+
+        $db = new SQLite3('donations.db');
+
+        // Définir la limite de résultats par page
+        $limit_per_page = 500;
+
+        $offset = ($nbpage - 1) * $limit_per_page;
+
+        // Utilisation de requête préparée pour éviter les injections SQL
+        $query = $db->prepare('SELECT * FROM donations ORDER BY donation_id LIMIT :limit OFFSET :offset');
+        $query->bindValue(':limit', $limit_per_page, SQLITE3_INTEGER);
+        $query->bindValue(':offset', $offset, SQLITE3_INTEGER);
+        $results = $query->execute();
     } else {
         // La valeur de 'page' n'est pas un entier valide entre 1 et 1000
         echo "La valeur de 'page' n'est pas valide. Veuillez entrer un nombre entre 1 et 1000.";
@@ -20,8 +31,6 @@ if (isset($_GET['page'])) {
     // La variable 'page' n'est pas présente dans l'URL
     echo "La variable 'page' n'est pas définie dans l'URL.";
 }
-$db = new SQLite3('donations.db');
-$results = $db->query('SELECT * FROM donations LIMIT 500');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -35,13 +44,13 @@ $results = $db->query('SELECT * FROM donations LIMIT 500');
         <tr>
             <th>Name</th>
             <th>Amount</th>
-            <th>Donation ID</th>
-            <th>Created At</th>
+            <th>Donation-ID</th>
+            <th>Created At (GMT-5)</th>
         </tr>
         <?php while ($row = $results->fetchArray(SQLITE3_BOTH)) { ?>
             <tr>
                 <td><?php echo $row["name"]; ?></td>
-                <td><?php echo $row["amount"]; ?></td>
+                <td><?php echo $row["amount"]; ?> €</td>
                 <td><?php echo $row["donation_id"]; ?></td>
                 <td><?php echo $row["created_at"]; ?></td>
             </tr>
